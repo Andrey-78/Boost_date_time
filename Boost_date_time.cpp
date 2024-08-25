3fb25a1863d71c58ef3b593c611729d128095d79
Andrey-78 <avk11121978@mail.ru>
2024-08-25 15:27:25 +0300

    Добавьте файлы проекта.

---
+++ Boost_date_time.cpp
@@ -0,0 +1,15 @@
+﻿#include<boost/date_time/gregorian/gregorian.hpp>
+#include<iostream>
+
+using namespace std;
+namespace gr = boost::gregorian;
+
+int main() {
+
+	gr::date locday = gr::day_clock::local_day();
+	gr::date birthday(1978, 12, 11);
+
+	gr::date_period dp(birthday, locday);
+
+	cout << dp.length() << endl;
+}
